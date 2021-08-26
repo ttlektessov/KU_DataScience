40f2345a08cf141ec122ff8687eea93ce7038380
Temirlan Tlektessov <58460135+ttlektessov@users.noreply.github.com>
2021-08-26 14:27:47 +0900

    HW 1: Math Review and Plotting

---
+++ hw1/tests/q6c.py
@@ -0,0 +1,4 @@
+test = {   'name': 'q6c',
+    'points': 2,
+    'suites': [{'cases': [{'code': '>>> prob_6c >= 0 and prob_6c <= 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+
